0dfdf436b2cbf33866cbaf684393efe044778ac2
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-11-08 09:47:02 -0500

    initial commit

---
+++ src/main/java/com/coveros/hello_world_two/EvenNumberGenerator.java
@@ -0,0 +1,13 @@
+package com.coveros.hello_world_two;
+import java.util.Random;
+
+public class EvenNumberGenerator {
+	public static int generateRandomEven() {
+		Random randomGenerator = new Random();
+		int result = randomGenerator.nextInt();
+		if(result%2!=0) {
+			result++;
+		}
+		return result;
+	}
+}
---
+++ target/hello_world_two/WEB-INF/classes/EvenNumberGenerator.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class EvenNumberGenerator {
+	public static int generateRandomEven() {
+		Random randomGenerator = new Random();
+		int result = randomGenerator.nextInt();
+		if(result%2!=0) {
+			result++;
+		}
+		return result;
+	}
+}

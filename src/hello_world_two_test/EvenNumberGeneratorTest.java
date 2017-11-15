fa46e5bba287e5fa4b6dfab3ab47751d28138d3a
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-11-15 09:53:40 -0500

    added junit test

---
+++ src/hello_world_two_test/EvenNumberGeneratorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import com.coveros.hello_world_two.EvenNumberGenerator;
+
+public class EvenNumberGeneratorTest{
+	
+	@Test
+	public void testRandomEvenNumberGenerator() {
+		int randomResult = EvenNumberGenerator.generateRandomEven();
+		assertEquals(0,randomResult%2);
+	}
+}
